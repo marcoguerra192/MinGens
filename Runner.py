1bcee71d79d6087b2c27f142dce60ee3b828df23
marcoguerra192 <marco.guerra192@gmail.com>
2025-01-23 15:42:40 +0000

    added a runner py file

---
+++ Runner.py
@@ -0,0 +1,25 @@
+## RUN THE COMPUTATION OF MIN GENS AND DEATH POINTS
+
+from ComputeMinGensDP import computeMinGensAndDeathPoints
+
+
+data_folder = os.path.join(os.getcwd(), 'data')
+output_folder = os.path.join(os.getcwd(), 'results')
+picture_folder = os.path.join(os.getcwd(), 'results')
+
+Thresh = 3.7
+plot = True
+deathpoints=True
+verbose=True
+square=False
+
+
+computeMinGensAndDeathPoints(Thresh = Thresh, 
+                             data_folder = data_folder,
+                             output_folder = output_folder,
+                             plot=plot,
+                             picture_folder=picture_folder, 
+                             deathpoints=deathpoints,
+                             verbose=verbose,
+                             square=square)
+
